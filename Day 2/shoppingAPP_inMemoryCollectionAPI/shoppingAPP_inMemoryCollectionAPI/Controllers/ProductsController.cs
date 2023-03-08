8d0b6409e254064b0ab8a8573a6855c0778c8a8c
Nikhil <nikhilshah.net@hotmail.com>
2023-03-08 10:36:54 +0530

    Day 2 - Inmemory API

---
+++ Day 2/shoppingAPP_inMemoryCollectionAPI/shoppingAPP_inMemoryCollectionAPI/Controllers/ProductsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using shoppingAPP_inMemoryCollectionAPI.Models;
+namespace shoppingAPP_inMemoryCollectionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        Products pObj = new Products(); //this is a bad code, we should use DI instead
+
+
+        #region Get Methods
+        [HttpGet]
+        [Route("productlist")]
+        public IActionResult GetAllProducts()
+        {
+            var result = pObj.GetAllProducts();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("productlist/{productId}")]
+        public IActionResult GetProductById(int productId)
+        {
+            try
+            {
+                var result = pObj.GetProductById(productId);
+                return Ok(result);
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+        #endregion
+
+
+        [HttpPost]
+        [Route("productlist/add")]
+        public IActionResult AddProduct([FromBody]Products newProduct)
+        {
+            var result = pObj.AddNewProduct(newProduct);
+
+            return Created("", result);
+        }
+
+        [HttpDelete]
+        [Route("productlist/delete/{productId}")]
+        public IActionResult DeleteProduct(int productId)
+        {
+            try
+            {
+                var result = pObj.DeleteProduct(productId);
+                return Accepted(result);
+            }
+            catch (Exception es)
+            {
+                return Ok(es.Message);
+                
+            }            
+        }
+
+        [HttpPut]
+        [Route("productId/edit")]
+        public IActionResult EditProduct([FromBody]Products changes)
+        {
+            try
+            {
+                var result = pObj.EditProduct(changes);
+                return Accepted(result);
+            }
+            catch (Exception es)
+            {
+
+                return Ok(es.Message);
+            }
+        }
+
+    }
+}
---
+++ Day 2/shoppingAPP_inMemoryCollectionAPI/shoppingAPP_inMemoryCollectionAPI/Models/Products.cs
@@ -0,0 +1,94 @@
+﻿namespace shoppingAPP_inMemoryCollectionAPI.Models
+{
+    public class Products
+    {
+        public int pId { get; set; }
+        public string pName { get; set; }
+        public string pCategory { get; set; }
+        public double pPrice { get; set; }
+        public bool pIsInStock { get; set; }
+
+        static List<Products> pList = new List<Products>()
+        {
+            new Products(){ pId=101, pName="Pepsi", pCategory="Cold-Drink", pIsInStock=true, pPrice=50},
+            new Products(){ pId=102, pName="IPhone", pCategory="Electronics", pIsInStock=true, pPrice=140000},
+            new Products(){ pId=103, pName="Maggie", pCategory="Fast-Food", pIsInStock=false, pPrice=75},
+            new Products(){ pId=104, pName="Appy", pCategory="Cold-Drink", pIsInStock=true, pPrice=125}
+        };
+
+        public List<Products> GetAllProducts()
+        {
+            return pList;
+        }
+
+        public Products GetProductById(int id)
+        {
+            var pr = pList.Find(p => p.pId == id);
+            if (pr != null)
+            {
+                return pr;
+            }
+            throw new Exception("Product Not found");
+        }
+
+        public string AddNewProduct(Products newProduct)
+        {
+            //do some data validataion if needed
+            pList.Add(newProduct);
+            return "Product Added Successfully";
+        }
+
+        public string DeleteProduct(int id)
+        {
+            var pr = pList.Find(p => p.pId == id);
+            if (pr != null)
+            {
+                pList.Remove(pr);
+                return "Product Deleted Successfully";
+            }
+            throw new Exception("Product Not found");
+        }
+
+        public string EditProduct(Products changes)
+        {
+            var pr = pList.Find(p => p.pId == changes.pId);
+            if (pr != null)
+            {
+                pr.pName = changes.pName;
+                pr.pCategory = changes.pCategory;
+                pr.pPrice = changes.pPrice;
+                pr.pIsInStock = changes.pIsInStock;
+
+                return "Product Edited Successfully";
+            }
+            throw new Exception("Product Not found");
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
