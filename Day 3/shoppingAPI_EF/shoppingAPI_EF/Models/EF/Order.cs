0a8b86b60cea591c42137af9ffada864a5895b65
Nikhil <nikhilshah.net@hotmail.com>
2023-03-09 11:01:16 +0530

    Day 3 - EF Core

---
+++ Day 3/shoppingAPI_EF/shoppingAPI_EF/Models/EF/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shoppingAPI_EF.Models.EF;
+
+public partial class Order
+{
+    public int OId { get; set; }
+
+    public string? OProduct { get; set; }
+
+    public int? OQty { get; set; }
+
+    public int? OValue { get; set; }
+}
---
+++ Day 3/shoppingAPI_EF/shoppingAPI_EF/Models/EF/ShoppingApiDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace shoppingAPI_EF.Models.EF;
+
+public partial class ShoppingApiDbContext : DbContext
+{
+    public ShoppingApiDbContext()
+    {
+    }
+
+    public ShoppingApiDbContext(DbContextOptions<ShoppingApiDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=shoppingAPI_DB;integrated security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OId).HasName("PK__orders__C2FECB3B8F36E98E");
+
+            entity.ToTable("orders");
+
+            entity.Property(e => e.OId)
+                .ValueGeneratedNever()
+                .HasColumnName("oId");
+            entity.Property(e => e.OProduct)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("oProduct");
+            entity.Property(e => e.OQty).HasColumnName("oQty");
+            entity.Property(e => e.OValue).HasColumnName("oValue");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.PId).HasName("PK__products__DD36D562EE0BF3DE");
+
+            entity.ToTable("products");
+
+            entity.Property(e => e.PId)
+                .ValueGeneratedNever()
+                .HasColumnName("pId");
+            entity.Property(e => e.PCategory)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("pCategory");
+            entity.Property(e => e.PIsInStock).HasColumnName("pIsInStock");
+            entity.Property(e => e.PName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("pName");
+            entity.Property(e => e.PPrice).HasColumnName("pPrice");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
