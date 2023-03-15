706224e04f5c16879f1453027690a583ce3dc0c6
Nikhil <nikhilshah.net@hotmail.com>
2023-03-15 10:43:02 +0530

    Day 6 - HttpClient call

---
+++ Day 6/HttpClientCalls/HttpClientCalls/Controllers/SecureApiCallsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HttpClientCalls.Models;
+namespace HttpClientCalls.Controllers
+{
+    public class SecureApiCallsController : Controller
+    {
+
+
+        // PostDataModel pObj = new PostDataModel(); //should use DI instead
+      private readonly  PostDataModel pObj;
+
+        public SecureApiCallsController(PostDataModel pObjREF)
+        {
+            this.pObj = pObjREF;
+        }
+
+        public IActionResult PostData()
+        {
+            ViewBag.post = pObj.GetPostData();
+            return View();
+        }
+    }
+}
---
+++ Day 6/HttpClientCalls/HttpClientCalls/Models/PostDataModel.cs
@@ -0,0 +1,39 @@
+﻿namespace HttpClientCalls.Models
+{
+    public class PostDataModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        List<PostDataModel> pList = new List<PostDataModel>();
+
+        public List<PostDataModel> GetPostData()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+
+            HttpClient client = new HttpClient();
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var call = client.GetAsync(url).Result;
+
+            if (call.IsSuccessStatusCode)
+            {
+                //we read the data
+
+                var data = call.Content.ReadAsAsync<List<PostDataModel>>();
+                data.Wait(); //this is for async calls, wait for all the data to come before return 
+                pList = data.Result;
+            }
+            else
+            {
+                throw new Exception("Could not get data, please contact Admin");
+            }
+
+            return pList;
+        }
+    }
+}
